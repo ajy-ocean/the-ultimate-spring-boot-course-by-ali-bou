8e4c1f1131577723c317910001f4b22d233f590d
Ajay Rawat <93385633+ajy-ocean@users.noreply.github.com>
2025-08-08 16:22:27 +0530

    initial commit

---
+++ src/main/java/com/the/ultimate/springboot/course/by/alibou/UltimateSpringbootCourseByAliBouApplication.java
@@ -0,0 +1,13 @@
+package com.the.ultimate.springboot.course.by.alibou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimateSpringbootCourseByAliBouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltimateSpringbootCourseByAliBouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/the/ultimate/springboot/course/by/alibou/UltimateSpringbootCourseByAliBouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.the.ultimate.springboot.course.by.alibou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimateSpringbootCourseByAliBouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
