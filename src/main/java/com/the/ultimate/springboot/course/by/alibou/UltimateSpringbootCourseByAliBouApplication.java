428e9b715bfa0fc6f091c7614ab77f6ad49e0aee
Ajay Rawat <93385633+ajy-ocean@users.noreply.github.com>
2025-08-11 13:00:43 +0530

    spring rest started

--- src/main/java/com/the/ultimate/springboot/course/by/alibou/UltimateSpringbootCourseByAliBouApplication.java
+++ src/main/java/com/the/ultimate/springboot/course/by/alibou/UltimateSpringbootCourseByAliBouApplication.java
@@ -3,21 +3,10 @@ package com.the.ultimate.springboot.course.by.alibou;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
-
 @SpringBootApplication
 public class UltimateSpringbootCourseByAliBouApplication {
 
 	public static void main(String[] args) {
-
-		var app = new SpringApplication(UltimateSpringbootCourseByAliBouApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "dev"));
-		var context = app.run(args);
-
-		MyFirstService myFirstService = context.getBean(MyFirstService.class);
-		System.out.println(myFirstService.tellAStory());
-		System.out.println(myFirstService.getCustomProperty());
-		System.out.println(myFirstService.getNumberCustomProperty());
+		SpringApplication.run(UltimateSpringbootCourseByAliBouApplication.class);
 	}
-
 }
